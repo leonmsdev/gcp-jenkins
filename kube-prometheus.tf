2f742eea80196c77b3a971ee6269b3b1bd161aa6
leonmsdev <leonms.dev@gmail.com>
2023-06-26 21:08:30 +0200

    chore(): change namespace reference

--- kube-prometheus.tf
+++ kube-prometheus.tf
@@ -2,12 +2,11 @@ resource "kubernetes_namespace" "prometheus" {
   metadata {
     name = "prometheus"
   }
-  depends_on = [google_container_node_pool.main_workload]
 }
 
 resource "helm_release" "kube-prometheus" {
   name       = "kube-prometheus-stack"
-  namespace  = kubernetes_namespace.prometheus
+  namespace  = kubernetes_namespace.prometheus.metadata[0].name
   version    = "9.3.1"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
