f1e809d5399238a621fb4d66b9704a6fa88131d9
leonmsdev <leonms.dev@gmail.com>
2023-06-26 21:18:45 +0200

    feat(): add new kube-prometheus-stack helm chart

--- kube-prometheus.tf
+++ kube-prometheus.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "prometheus" {
 resource "helm_release" "kube-prometheus" {
   name       = "kube-prometheus-stack"
   namespace  = kubernetes_namespace.prometheus.metadata[0].name
-  version    = "9.3.1"
-  repository = "https://prometheus-community.github.io/helm-charts"
+  version    = "47.0.0"
+  repository = "https://github.com/prometheus-operator/kube-prometheus"
   chart      = "kube-prometheus-stack"
 }
