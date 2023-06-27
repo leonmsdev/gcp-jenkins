ba08371d7e4b0f5f31877b662e6b9ac778590e8c
leonmsdev <leonms.dev@gmail.com>
2023-06-27 20:21:50 +0200

    feat(dns):create cloud managed dns zone

---
+++ dns.tf
@@ -0,0 +1,13 @@
+locals {
+  dns_name = "leonschmidt-cloud"
+}
+
+resource "google_dns_managed_zone" "leonschmidt_cloud" {
+  name        = local.dns_name
+  dns_name    = format("%s.com.",local.dns_name)
+  description = "leonschmidt-cloud DNS zone"
+
+  labels = {
+    scope = "kubernetes"
+  }
+}
