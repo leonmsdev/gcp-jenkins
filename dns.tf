2a7d5267f5b011cb17e551a7bcd553d086bb601b
leonmsdev <leonms.dev@gmail.com>
2023-06-27 20:23:19 +0200

    chore(fmt): do terraform formating

--- dns.tf
+++ dns.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "google_dns_managed_zone" "leonschmidt_cloud" {
   name        = local.dns_name
-  dns_name    = format("%s.com.",local.dns_name)
+  dns_name    = format("%s.com.", local.dns_name)
   description = "leonschmidt-cloud DNS zone"
 
   labels = {
