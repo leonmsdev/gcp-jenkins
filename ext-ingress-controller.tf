7ee6712a7f9a75bb37de219f6ff80203a5443136
leonmsdev <leonms.dev@gmail.com>
2023-07-02 18:56:30 +0200

    feat(ingress):delete ext ingress

--- ext-ingress-controller.tf
+++ ext-ingress-controller.tf
@@ -4,14 +4,14 @@ resource "kubernetes_namespace" "ext_ingress_nginx" {
   }
 }
 
-resource "helm_release" "ext_ingress_nginx" {
-  name       = "ext"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = kubernetes_namespace.ext_ingress_nginx.metadata[0].name
-  version    = ">=3.0.0"
+# resource "helm_release" "ext_ingress_nginx" {
+#   name       = "ext"
+#   repository = "https://kubernetes.github.io/ingress-nginx"
+#   chart      = "ingress-nginx"
+#   namespace  = kubernetes_namespace.ext_ingress_nginx.metadata[0].name
+#   version    = ">=3.0.0"
 
-  values = [
-    file("${path.module}/configs/ext-nginx-values.yml")
-  ]
-}
+#   values = [
+#     file("${path.module}/configs/ext-nginx-values.yml")
+#   ]
+# }
