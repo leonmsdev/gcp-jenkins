afd3e25d17c93f278b6d04aba2c3d6b8a50ccbe7
leonmsdev <leonms.dev@gmail.com>
2023-07-02 20:30:51 +0200

    feat(): add index to the secret

--- dependabot.tf
+++ dependabot.tf
@@ -8,13 +8,14 @@
 #     name = "dependabot"
 #   }
 # }
+
 locals {
   dependabot_secrets = {
-    #defines secret name and version and the kubernetes key inside the secret                 
-    "dependabot-gitlab-access-token" = [1, "token"]
-    "dependabot-github-access-token" = [1, "token"]
-    "dependabot-mongodb-passwd"      = [1, "passwd"]
-    "dependabot-redis-passwd"        = [1, "passwd"]
+    # Defines [0] index, [1] secret version, [2] the version to use and the kubernetes key name inside the secret.
+    "dependabot-gitlab-access-token" = [0, 1, "token"], # [0 , 1, 2] *The first index needs to be +1 if a new elemt gets added to the list.
+    "dependabot-github-access-token" = [1, 1, "token"],
+    "dependabot-mongodb-passwd"      = [2, 1, "passwd"],
+    "dependabot-redis-passwd"        = [3, 1, "passwd"]
   }
 }
 
@@ -22,7 +23,7 @@ data "google_secret_manager_secret_version" "dependabot_secrets" {
   for_each = local.dependabot_secrets
   secret   = each.key
   project  = var.gcp_project_id
-  version  = each.value[0]
+  version  = each.value[1]
 }
 
 resource "kubernetes_secret" "dependabot_gitlab_access_token_secret" {
@@ -34,6 +35,6 @@ resource "kubernetes_secret" "dependabot_gitlab_access_token_secret" {
   }
 
   data = {
-    each.value[1] = data.google_secret_manager_secret_version.dependabot_secrets[each.index].secret_data
+    each.value[2] = data.google_secret_manager_secret_version.dependabot_secrets[each.value[0]].secret_data
   }
 }
