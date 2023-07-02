98a88e7cc3c950437f84f7b117e11ed3400c2694
leonmsdev <leonms.dev@gmail.com>
2023-07-02 20:14:30 +0200

    feat(): generate dependabot kubernetes secret

--- dependabot.tf
+++ dependabot.tf
@@ -8,3 +8,32 @@
 #     name = "dependabot"
 #   }
 # }
+locals {
+  dependabot_secrets = {
+    #defines secret name and version and the kubernetes key inside the secret                 
+    "dependabot-gitlab-access-token" = [1, "token"]
+    "dependabot-github-access-token" = [1, "token"]
+    "dependabot-mongodb-passwd"      = [1, "passwd"]
+    "dependabot-redis-passwd"        = [1, "passwd"]
+  }
+}
+
+data "google_secret_manager_secret_version" "dependabot_secrets" {
+  for_each = local.dependabot_secrets
+  secret   = each.key
+  project  = var.gcp_project_id
+  version  = each.value
+}
+
+resource "kubernetes_secret" "dependabot_gitlab_access_token_secret" {
+  for_each = local.dependabot_secrets
+
+  metadata {
+    name      = trimprefix(each.key, "dependabot-")
+    namespace = "dependabot"
+  }
+
+  data = {
+    each.value[1] = data.google_secret_manager_secret_version.dependabot_github_access_token.secret_data
+  }
+}
