712f4fd0c737fc79ae498341e5e00fe200186192
leonmsdev <leonms.dev@gmail.com>
2023-07-02 16:24:56 +0200

    refactor(secret): add list to generate secret for every value in locals

--- secret-manager.tf
+++ secret-manager.tf
@@ -1,3 +1,12 @@
+locals {
+  secrets = [
+    "dependabot-redis-passwd",
+    "dependabot-mongodb-passwd",
+    "dependabot-gitlab-access-token",
+    "dependabot-github-access-token",
+  ]
+}
+
 resource "google_project_service" "secret_manager" {
   project = var.gcp_project_id
   service = "secretmanager.googleapis.com"
@@ -9,10 +18,11 @@ resource "google_service_account" "secret_manager_sa" {
 }
 
 resource "google_secret_manager_secret" "admin-password" {
+    for_each = local.secrets
   #  provider = google-beta
   depends_on = [google_project_service.secret_manager]
 
-  secret_id = "dependabot-redis"
+  secret_id = each.value
   replication {
     user_managed {
       replicas {
