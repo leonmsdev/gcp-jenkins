4bac987cee3b83365a168972f6ec21b52a76bfe5
leonmsdev <leonms.dev@gmail.com>
2023-07-02 16:59:40 +0200

    chore(): comment out  namespace

--- dependabot.tf
+++ dependabot.tf
@@ -1,10 +1,10 @@
-import {
-  to = kubernetes_namespace.dependabot
-  id = "3ebef761-ef77-46ae-86cc-3d12313d0b0b"
-}
+# import {
+#   to = kubernetes_namespace.dependabot
+#   id = "3ebef761-ef77-46ae-86cc-3d12313d0b0b"
+# }
 
-resource "kubernetes_namespace" "dependabot" {
-  metadata {
-    name = "dependabot"
-  }
-}
+# resource "kubernetes_namespace" "dependabot" {
+#   metadata {
+#     name = "dependabot"
+#   }
+# }
