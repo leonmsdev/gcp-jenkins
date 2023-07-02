67f9502e4b76e34e4b0e1aa7fd22b7cec4fdbb8a
leonmsdev <leonms.dev@gmail.com>
2023-07-02 15:45:01 +0200

    chore(import): try to import terraform dependabot namespace

---
+++ dependabot.tf
@@ -0,0 +1,15 @@
+import {
+    to = kubernetes_namespace.dependabot
+    id = random_integer.import_id.result
+}
+
+resource "random_integer" "import_id" {
+  byte_length = 4
+}
+
+resource "kubernetes_namespace" "dependabot" {
+  metadata {
+    name = "dependabot"
+  }
+}
+
