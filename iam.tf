9f093e1a9a59fc1206209393dee55ccba7e3925d
leonmsdev <leonms.dev@gmail.com>
2023-07-02 15:34:11 +0200

    feat(secret): configure google secret manager and create dependabot-redis secret

---
+++ iam.tf
@@ -0,0 +1,20 @@
+locals {
+  gcp_bindings = {
+    "roles/secretmanager.admin" = [
+      format("serviceAccount:%s", google_service_account.secret_manager_sa.email)
+    ],
+  }
+}
+
+resource "google_project_iam_policy" "gcp_project_iam" {
+  project     = var.gcp_project_id
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
+data "google_iam_policy" "google_iam" {
+  for_each = local.gcp_bindings
+  binding {
+    role    = each.key
+    members = each.value
+  }
+}
---
+++ secret-manager.tf
@@ -0,0 +1,23 @@
+resource "google_project_service" "secret_manager" {
+  project = var.gcp_project_id
+  service = "secretmanager.googleapis.com"
+}
+
+resource "google_service_account" "secret_manager_sa" {
+  account_id   = "secret-manager"
+  display_name = "Secret Manager SA"
+}
+
+resource "google_secret_manager_secret" "admin-password" {
+#  provider = google-beta
+  depends_on = [ google_project_service.google_project_service.secret_manager ]
+  
+  secret_id   = "dependabot-redis"
+  replication {
+    user_managed {
+      replicas {
+        location = "europe-west4"
+      }
+    }
+  }
+}
