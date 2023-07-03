dc4854baf3c22210f6ef218d73224beacaa79397
leonmsdev <leonms.dev@gmail.com>
2023-07-03 19:33:20 +0200

    chore(fmt):do terraform format

--- dependabot.tf
+++ dependabot.tf
@@ -1,25 +1,25 @@
 data "google_secret_manager_secret_version" "dependabo_gitlab_access_token" {
-  secret   = "dependabot-gitlab-access-token"
-  project  = var.gcp_project_id
-  version  = 1
+  secret  = "dependabot-gitlab-access-token"
+  project = var.gcp_project_id
+  version = 1
 }
 
 data "google_secret_manager_secret_version" "dependabo_github_access_token" {
-  secret   = "dependabot-github-access-token"
-  project  = var.gcp_project_id
-  version  = 1
+  secret  = "dependabot-github-access-token"
+  project = var.gcp_project_id
+  version = 1
 }
 
 data "google_secret_manager_secret_version" "dependabot_mongodb_passwd" {
-  secret   = "dependabot-mongodb-passwd"
-  project  = var.gcp_project_id
-  version  = 1
+  secret  = "dependabot-mongodb-passwd"
+  project = var.gcp_project_id
+  version = 1
 }
 
 data "google_secret_manager_secret_version" "dependabot_redis_passwd" {
-  secret   = "dependabot-redis-passwd"
-  project  = var.gcp_project_id
-  version  = 1
+  secret  = "dependabot-redis-passwd"
+  project = var.gcp_project_id
+  version = 1
 }
 
 resource "kubernetes_secret" "dependabot_gitlab_access_token_secret" {
@@ -30,7 +30,7 @@ resource "kubernetes_secret" "dependabot_gitlab_access_token_secret" {
   data = {
     SETTINGS__GITLAB_ACCESS_TOKEN = data.google_secret_manager_secret_version.dependabo_gitlab_access_token.secret_data,
     SETTINGS__GITHUB_ACCESS_TOKEN = data.google_secret_manager_secret_version.dependabo_github_access_token.secret_data,
-    REDIS_PASSWORD = data.google_secret_manager_secret_version.dependabot_redis_passwd.secret_data,
-    MONGODB_PASSWORD = data.google_secret_manager_secret_version.dependabot_mongodb_passwd.secret_data
+    REDIS_PASSWORD                = data.google_secret_manager_secret_version.dependabot_redis_passwd.secret_data,
+    MONGODB_PASSWORD              = data.google_secret_manager_secret_version.dependabot_mongodb_passwd.secret_data
   }
 }
