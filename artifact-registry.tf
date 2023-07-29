ea40133031c5e550063dcc1f2c8a32845602dde8
leonmsdev <leonms.dev@gmail.com>
2023-07-29 13:46:24 +0200

    feat(ar): activate artifact registry and generate docker repository

---
+++ artifact-registry.tf
@@ -0,0 +1,13 @@
+resource "google_project_service" "artifact_registry" {
+  project = var.gcp_project_id
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_artifact_registry_repository" "hochzeitsauto_schwerin" {
+  location      = var.gcp_zone
+  repository_id = "hochzeitsauto-schwerin"
+  description   = "Holds Docker Builds of hochzeitsauto-schwerin"
+  format        = "DOCKER"
+
+  depends_on = [ google_project_service.artifact_registry ]
+}
